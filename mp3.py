8abd5f241a98629870fb97644849e0f3533eb6ed
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2016-10-13 15:08:02 +0300

    mp3: convert to python (sequential encoding for now)

---
+++ mp3.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import subprocess
+
+import meta
+
+def usage_and_exit():
+    print("""echo mp3: encode rus audio track for Goswami Maharaj's video from wav to mp3
+echo usage: mp3 "yyyy-mm-dd goswamimj.mp4"
+echo (or drag and drop the file onto me)""")
+    exit()
+
+
+def mp3(filename):
+    rus_mixdown_wav = meta.get_work_filename(filename, '_rus_mixdown.wav')
+
+    cmd = ['ffmpeg', '-y',
+           '-i', rus_mixdown_wav,
+           '-codec:a', 'mp3',
+           '-b:a', '128k']
+    cmd += meta.get_ss_args(filename)
+    cmd += meta.ffmpeg_meta_args_rus_stereo(filename)
+    cmd += [meta.get_work_filename(filename, '_rus_stereo.mp3')]
+    subprocess.run(cmd, check=True)
+
+    cmd = ['ffmpeg', '-y',
+           '-i', rus_mixdown_wav,
+           '-codec:a', 'mp3',
+           '-ac', '1',
+           '-b:a', '96k']
+    cmd += meta.get_ss_args(filename)
+    cmd += meta.ffmpeg_meta_args_rus_mono(filename)
+    cmd += [meta.get_work_filename(filename, '_rus_mono.mp3')]
+    subprocess.run(cmd, check=True)
+
+    cmd = ['ffmpeg', '-y',
+           '-i', meta.get_work_filename(filename, '.m4a'),
+           '-ac', '1',
+           '-codec:a', 'mp3', '-b:a', '96k']
+    cmd += meta.get_ss_args(filename)
+    cmd += meta.ffmpeg_meta_args(filename)
+    cmd += [meta.get_work_filename(filename, '_eng.mp3')]
+    subprocess.run(cmd, check=True)
+
+def main():
+    try:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print('file "%s" not found' % filename)
+            print('')
+            usage_and_exit()
+        mp3(filename)
+    except IndexError:
+        usage_and_exit()
+
+if __name__ == '__main__':
+    main()
