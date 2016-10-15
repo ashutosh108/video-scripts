7c2c82ecdc1d413ceec65438c3dc4d23af0e1c9e
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2016-10-15 16:28:14 +0300

    refactor meta: start non-public methods with underscore

--- meta.py
+++ meta.py
@@ -4,22 +4,22 @@ import yaml
 import datetime
 import string
 
-_yaml_data = {}
+_yaml_data_cache = {}
 
 
-def yaml_data(filename):
-    global _yaml_data
-    if filename not in _yaml_data:
+def _yaml_data(filename):
+    global _yaml_data_cache
+    if filename not in _yaml_data_cache:
         try:
             yaml_filename = os.path.splitext(filename)[0] + '.yml'
             with open(yaml_filename, 'r', encoding='UTF-8') as f:
-                _yaml_data[filename] = yaml.load(f)
+                _yaml_data_cache[filename] = yaml.load(f)
         except (IndexError, FileNotFoundError) as e:
-            _yaml_data[filename] = dict()
-    return _yaml_data[filename]
+            _yaml_data_cache[filename] = dict()
+    return _yaml_data_cache[filename]
 
 
-def get_skip_time(filename: str) -> str:
+def _get_skip_time(filename: str) -> str:
     """
     get proper argument for -ss min:sec part of the ffmpeg command line
     e.g. '2:51' if that's what's written in filename_offset.txt
@@ -34,7 +34,7 @@ def get_skip_time(filename: str) -> str:
                 return f.readline().rstrip()
         except FileNotFoundError:
             pass
-    y = yaml_data(filename)
+    y = _yaml_data(filename)
     try:
         return str(datetime.timedelta(seconds=y['skip']))
     except KeyError:
@@ -42,37 +42,30 @@ def get_skip_time(filename: str) -> str:
 
 
 def get_ss_args(filename):
-    skip_time = get_skip_time(filename)
+    skip_time = _get_skip_time(filename)
     if skip_time:
         return ['-ss', skip_time]
     else:
         return []
 
 
-def get_title_for_file(filename):
-    basename = os.path.basename(filename)
-    basename_wo_ext = os.path.splitext(basename)[0]
-    title = basename_wo_ext.replace(' goswamimj', '')
-    return title
-
-
-def get_title_eng(filename):
-    y = yaml_data(filename)
+def _get_title_eng(filename):
+    y = _yaml_data(filename)
     try:
         return y['title_eng']
     except KeyError:
         return os.path.basename(os.path.splitext(filename)[0])
 
 
-def get_title_rus(filename):
-    y = yaml_data(filename)
+def _get_title_rus(filename):
+    y = _yaml_data(filename)
     try:
         return y['title_rus']
     except KeyError:
         return os.path.basename(os.path.splitext(filename)[0])
 
 
-def artist_real_name(artist):
+def _artist_real_name(artist):
     known_artists = dict(
         goswamimj='Bhakti Sudhir Goswami',
         bsgoswami='Bhakti Sudhir Goswami',
@@ -88,7 +81,7 @@ def artist_real_name(artist):
     return artist
 
 
-def artist_real_name_rus(artist):
+def _artist_real_name_rus(artist):
     known_artists = dict(
         goswamimj='Бхакти Судхир Госвами',
         bsgoswami='Бхакти Судхир Госвами',
@@ -104,33 +97,33 @@ def artist_real_name_rus(artist):
     return artist
 
 
-def get_artist_eng(filename):
+def _get_artist_eng(filename):
     basename = os.path.basename(filename)
     match = re.match('^(\d\d\d\d)-?(\d\d)-?(\d\d)\s+(.*)\.', basename)
     if match is not None:
         artists_str = match.group(4)
         artists = []
         for artist in artists_str.split('_'):
-            artists.append(artist_real_name(artist))
+            artists.append(_artist_real_name(artist))
         return ', '.join(artists)
     else:
         return 'Unknown'
 
 
-def get_artist_rus(filename):
+def _get_artist_rus(filename):
     basename = os.path.basename(filename)
     match = re.match('^(\d\d\d\d)-?(\d\d)-?(\d\d)\s+(.*)\.', basename)
     if match is not None:
         artists_str = match.group(4)
         artists = []
         for artist in artists_str.split('_'):
-            artists.append(artist_real_name_rus(artist))
+            artists.append(_artist_real_name_rus(artist))
         return ', '.join(artists)
     else:
         return 'Автор неизвестен'
 
 
-def get_year_month_day(filename):
+def _get_year_month_day(filename):
     basename = os.path.basename(filename)
     match = re.match('^(\d\d\d\d)-?(\d\d)-?(\d\d)', basename)
     if match:
@@ -139,9 +132,9 @@ def get_year_month_day(filename):
 
 
 def ffmpeg_meta_args(filename):
-    title = get_title_eng(filename)
-    artist = get_artist_eng(filename)
-    [year, month, day] = get_year_month_day(filename)
+    title = _get_title_eng(filename)
+    artist = _get_artist_eng(filename)
+    [year, month, day] = _get_year_month_day(filename)
     args = [
        '-metadata', 'artist=' + artist,
        '-metadata', 'title=' + title,
@@ -154,9 +147,9 @@ def ffmpeg_meta_args(filename):
 
 
 def ffmpeg_meta_args_rus_mono(filename):
-    title = get_title_rus(filename) + ' (моно)'
-    artist = get_artist_rus(filename)
-    [year, month, day] = get_year_month_day(filename)
+    title = _get_title_rus(filename) + ' (моно)'
+    artist = _get_artist_rus(filename)
+    [year, month, day] = _get_year_month_day(filename)
     args = [
        '-metadata', 'artist=' + artist,
        '-metadata', 'title=' + title,
@@ -169,9 +162,9 @@ def ffmpeg_meta_args_rus_mono(filename):
 
 
 def ffmpeg_meta_args_rus_stereo(filename):
-    title = get_title_rus(filename)
-    artist = get_artist_rus(filename)
-    [year, month, day] = get_year_month_day(filename)
+    title = _get_title_rus(filename)
+    artist = _get_artist_rus(filename)
+    [year, month, day] = _get_year_month_day(filename)
     args = [
        '-metadata', 'artist=' + artist,
        '-metadata', 'title=' + title,
@@ -191,35 +184,35 @@ def get_work_filename(filename, add):
 
 
 def get_youtube_title_eng(filename):
-    title = get_title_eng(filename)
+    title = _get_title_eng(filename)
     if title[-1] not in string.punctuation:
         title += '.'
-    artist = get_artist_eng(filename)
+    artist = _get_artist_eng(filename)
     return title + ' ' + artist
 
 
-def get_description_eng(filename):
+def _get_description_eng(filename):
     try:
-        return yaml_data(filename)['descr_eng']
+        return _yaml_data(filename)['descr_eng']
     except KeyError:
         return ''
 
 
-def srila_maharaj_eng(name):
+def _srila_maharaj_eng(name):
     return 'Srila ' + name + ' Maharaj'
 
 
-def get_author_with_title_eng(filename):
-    authors = get_artist_eng(filename).split(', ')
-    return ', '.join(map(srila_maharaj_eng, authors))
+def _get_author_with_title_eng(filename):
+    authors = _get_artist_eng(filename).split(', ')
+    return ', '.join(map(_srila_maharaj_eng, authors))
 
 
 def get_youtube_description_eng(filename):
-    year, month, day = get_year_month_day(filename)
+    year, month, day = _get_year_month_day(filename)
     dt_obj = datetime.date(int(year), int(month), int(day))
-    author_with_title = get_author_with_title_eng(filename)
+    author_with_title = _get_author_with_title_eng(filename)
     date = '{dt:%B} {dt.day}, {dt:%Y}'.format(dt=dt_obj)
-    yt_descr = get_description_eng(filename) + '\n'
+    yt_descr = _get_description_eng(filename) + '\n'
     yt_descr += author_with_title + '\n'  # e.g. Srila Bhakti Rañjan Madhusudan Maharaj
     yt_descr += date + '\n'  # e.g. October 11, 2016
     yt_descr += 'Theistic Media Studios, Gupta Govardhan Ashram.\n'
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -8,38 +8,32 @@ import os
 
 class test_meta(TestCase):
     def test_get_ss_arg_for_file_nonexisting(self):
-        self.assertEqual(meta.get_skip_time('qwe'), None)
+        self.assertEqual(meta._get_skip_time('qwe'), None)
 
     def test_get_ss_arg_for_file_existing(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 goswamimj.mp4')
-        self.assertEqual(meta.get_skip_time(filename), '1:15')
-
-    def test_get_title_for_file(self):
-        dir = os.path.dirname(__file__)
-        filename = os.path.join(dir, 'files', '2016-10-07 goswamimj.mp4')
-        self.assertEqual('2016-10-07', meta.get_title_for_file(filename))
-
+        self.assertEqual(meta._get_skip_time(filename), '1:15')
 
     def test_get_artist_eng(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 goswamimj.mp4')
-        self.assertEqual('Bhakti Sudhir Goswami', meta.get_artist_eng(filename))
+        self.assertEqual('Bhakti Sudhir Goswami', meta._get_artist_eng(filename))
 
     def test_get_artist_eng_multiple(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 janardanmj_goswamimj.mp4')
-        self.assertEqual('Bhakti Pavan Janardan, Bhakti Sudhir Goswami', meta.get_artist_eng(filename))
+        self.assertEqual('Bhakti Pavan Janardan, Bhakti Sudhir Goswami', meta._get_artist_eng(filename))
 
     def test_get_artist_eng_ranjan(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 brmadhusudan.mp4')
-        self.assertEqual('Bhakti Rañjan Madhusudan', meta.get_artist_eng(filename))
+        self.assertEqual('Bhakti Rañjan Madhusudan', meta._get_artist_eng(filename))
 
     def test_get_year_month_day(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 brmadhusudan.mp4')
-        [year, month, day] = meta.get_year_month_day(filename)
+        [year, month, day] = meta._get_year_month_day(filename)
         self.assertEqual('2016', year)
         self.assertEqual('10', month)
         self.assertEqual('07', day)
@@ -47,19 +41,19 @@ class test_meta(TestCase):
     def test_get_title_eng(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 goswamimj.mp4')
-        title_eng = meta.get_title_eng(filename)
+        title_eng = meta._get_title_eng(filename)
         self.assertEqual('Have faith in Guru-Vaishnava, not in yourself', title_eng)
 
     def test_get_skip_time(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-07 goswamimj.mp4')
-        skip_time = meta.get_skip_time(filename)
+        skip_time = meta._get_skip_time(filename)
         self.assertEqual('1:15', skip_time)
 
     def test_get_skip_time_from_yml(self):
         dir = os.path.dirname(__file__)
         filename = os.path.join(dir, 'files', '2016-10-12 brmadhusudan.mp4')
-        skip_time = meta.get_skip_time(filename)
+        skip_time = meta._get_skip_time(filename)
         self.assertEqual('0:07:56', skip_time)
 
     def test_get_youtube_description_eng(self):
