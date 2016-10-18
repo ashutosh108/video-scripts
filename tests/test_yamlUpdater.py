baed63a0db2e166c3460a6d138663a23f94d8783
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2016-10-18 14:45:36 +0300

    test_yamlUpdater: make it pass realiably. Added workaround for some process (windows antivurus??) preventing us from removing or creating our temporary file for a short time.  Waiting for 0.1 seconds and retrying helps. Go figure.

--- tests/test_yamlUpdater.py
+++ tests/test_yamlUpdater.py
@@ -2,40 +2,109 @@ from unittest import TestCase
 import os
 import yaml
 import yamlupdater
+import sys
+import inspect
+import time
 
 
 class TestYamlUpdater(TestCase):
+    DEBUG = False
+
+    @staticmethod
+    def begin():
+        if TestYamlUpdater.DEBUG:
+            caller = inspect.stack()[1][3]
+            print('{%s...' % caller, end='')
+            sys.stdout.flush()
+
+    @staticmethod
+    def mark(msg):
+        if TestYamlUpdater.DEBUG:
+            caller = inspect.stack()[1][3]
+            print('%s.%s' % (caller, msg), end='')
+            sys.stdout.flush()
+
+    @staticmethod
+    def end():
+        if TestYamlUpdater.DEBUG:
+            caller = inspect.stack()[1][3]
+            print(' %s}' % caller)
+            sys.stdout.flush()
+
     def setUp(self):
+        self.begin()
         self.filename = os.path.join(os.path.dirname(__file__), 'files', 'TestYamlUpdater.yml')
+        retry_count = 0
+        while True:
+            try:
+                self.f = open(self.filename, 'w')
+                break
+            except PermissionError:
+                if retry_count == 5:
+                    raise
+                self.mark('retrying...')
+                retry_count += 1
+                time.sleep(0.1)
+        self.end()
 
     def tearDown(self):
-        os.unlink(self.filename)
+        self.begin()
+        self.f.close()
+        retry_count = 0
+        while True:
+            try:
+                os.unlink(self.filename)
+            except PermissionError:
+                if retry_count == 5:
+                    raise
+                self.mark('retrying...')
+                retry_count += 1
+                time.sleep(0.1)
+            else: break
+        self.end()
 
     def test_set(self):
-        with open(self.filename, 'w') as f:
-            f.write('key: 0')
+        self.begin()
+        self.f.write('key: 0')
+        self.f.close()
+        self.mark('init completed...')
         yamlupdater.set(self.filename, 'key', 1)
+        self.mark('yaml update completed...')
 
-        d2 = yaml.load(open(self.filename, 'r'))
+        self.f = open(self.filename, 'r')
+        d2 = yaml.load(self.f)
         self.assertEqual(1, d2['key'])
+        self.end()
 
     def test_set_saves_without_braces(self):
-        with open(self.filename, 'w') as f:
-            f.write('key: 1')
+        self.begin()
+        self.f.write('key: 1')
+        self.f.close()
+        self.mark('file init completed...')
         yamlupdater.set(self.filename, 'key', 0)
-        with open(self.filename, 'r') as f:
-            self.assertEqual('key: 0\n', f.read())
+        self.mark('yaml update completed...')
+        self.f = open(self.filename, 'r')
+        self.assertEqual('key: 0\n', self.f.read())
+        self.end()
 
     def test_set_keeps_strings_unescaped(self):
-        with open(self.filename, 'w') as f:
-            f.write('title_eng: Test title')
+        self.begin()
+        self.f.write('title_eng: Test title')
+        self.f.close()
+        self.mark('file init completed...')
         yamlupdater.set(self.filename, 'key', 1)
-        with open(self.filename, 'r') as f:
-            self.assertEqual('title_eng: Test title\nkey: 1\n', f.read())
+        self.mark('yaml update completed...')
+        self.f = open(self.filename, 'r')
+        self.assertEqual('title_eng: Test title\nkey: 1\n', self.f.read())
+        self.end()
 
     def test_set_keeps_text_blocks_same_pipe_type(self):
-        with open(self.filename, 'w') as f:
-            f.write('descr_eng: |\n    - line1\n    - line2\n')
+        self.begin()
+        self.f.write('descr_eng: |\n    - line1\n    - line2\n')
+        self.f.close()
+        self.mark('file init completed...')
         yamlupdater.set(self.filename, 'key', 1)
-        with open(self.filename, 'r') as f:
-            self.assertEqual('descr_eng: |\n    - line1\n    - line2\nkey: 1\n', f.read())
+        self.mark('yaml update completed...')
+        self.f = open(self.filename, 'r')
+        self.assertEqual('descr_eng: |\n    - line1\n    - line2\nkey: 1\n', self.f.read())
+        self.end()
