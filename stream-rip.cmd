416eaf1d1edf40f8dc0f44e365d93f951373c0e9
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2016-12-17 11:14:20 +0200

    Test version of stream-ripper command

---
+++ stream-rip.cmd
@@ -0,0 +1,6 @@
+rem http://streamripper.sourceforge.net/
+"C:\Program Files (x86)\Streamripper\streamripper.exe" http://hyades.shoutca.st:8350/listen.pls?sid=1 -u "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36" -A -a
+REM http://hyades.shoutca.st:8350/listen.pls?sid=1
+rem http://yp.shoutcast.com/sbin/tunein-station.pls?id=830692
+REM   -a [file]      - Rip to single file, default name is timestamped
+REM   -A             - Don't write individual tracks
