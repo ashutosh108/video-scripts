e743b56f5ce2a92948a9aced5c831aa4469e0bc0
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2017-01-06 23:00:00 +0200

    stream-rip: add command to save streams to separate mp3s using metadata (one file per track/class)

--- stream-rip.cmd
+++ stream-rip.cmd
@@ -6,3 +6,4 @@ rem http://hyades.shoutca.st:8350/index.html?sid=1
 REM   -a [file]      - Rip to single file, default name is timestamped
 REM   -A             - Don't write individual tracks
 rem sleep $(($(date -d 14:30 +%s)-$(date +%s))) && while :; do "C:\Program Files (x86)\Streamripper\streamripper.exe" http://hyades.shoutca.st/tunein/tms.pls -u "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36" -A -a; sleep 15; done
+rem sleep $(($(date -d 14:30 +%s)-$(date +%s))) && while :; do "C:\Program Files (x86)\Streamripper\streamripper.exe" http://hyades.shoutca.st/tunein/tms.pls -u "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"; sleep 15; done
