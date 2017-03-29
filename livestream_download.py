d0fa8e48d65695ca8d94080918985e3830d76520
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2017-03-29 15:27:33 +0300

    WIP: ground work in livestream_download

--- livestream_download.py
+++ livestream_download.py
@@ -1,5 +1,19 @@
-def main():
+import urllib.request
+
+ACCOUNT_URL = 'https://livestream.com/accounts/2645002/'
+
+
+def get_next_event_url(account_url):
+    f = urllib.request.urlopen(account_url)
+    print(f.read())
+
+def start_downloader_for_next_event(account_url):
+    event_url = get_next_event_url(account_url)
     pass
+
+
+def main():
+    start_downloader_for_next_event(ACCOUNT_URL)
 
 if __name__ == '__main__':
     main()
